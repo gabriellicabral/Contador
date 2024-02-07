d6cc73ab0773ce245a148c132b975c05f36c5fee
gabriellicabral <gabrielli.cabral2017@gmail.com>
2024-02-07 15:30:15 -0300

    estilizando o contador

--- src/componentes/Contador/Contador.jsx
+++ src/componentes/Contador/Contador.jsx
@@ -1,14 +1,32 @@
 import React, {useState} from "react"
+import "./Contador.css"
 
 function Contador (){
     const [numero, setNumero] = useState(0)
+
+function Adicionar (){
+    if(numero < 20){
+    setNumero (numero + 1)
+    }
+}
+function Remover (){
+    if(numero > 0 ){
+    setNumero (numero - 1)
+}
+}
+function Clear (){
+    setNumero(0)
+}
     return (
-        <>
-        <h1>{numero}</h1>
-        <buttom>+</buttom>
-        <buttom>-</buttom>
-        <buttom>C</buttom>
-        </>
+        <section class="contador">
+        <h1 class ="numero">{numero}</h1>
+        <div class ="boxButton">
+        <button onClick = {Adicionar}>+</button>
+        <button onClick = {Remover}>-</button>
+        <button onClick = {Clear}>C</button>
+        </div>
+        </section>
     )
 }
+
 export default Contador
